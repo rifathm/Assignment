f2be43c1254e437788f4e8b84f125fe1b87dfeef
rifathm <cscuser@DESKTOP-HP35111.cssamba.jfn.ac.lk>
2020-09-18 05:40:09 +0530

    created structure for CscExample.java

---
+++ CscExample.java
@@ -0,0 +1,46 @@
+public class CscExample{
+
+private int questionNo;
+private String category;
+private String text;
+
+
+	public void setQuestionNo(int questionNo)
+	{
+	}
+	public void setCategory(String category)
+	{
+	}
+
+	public void setText(String text)
+	{	
+	}
+
+
+
+        public int getQuestionNo(){
+                }
+
+        public String getCategory(){
+              }
+
+        public String getText()
+	{}
+
+	public void addQuestion(CscExample qu)
+	{
+	}
+
+	public void removeQuestion(int quId)
+	{
+	}
+	
+	public void editQuestion(int quId)
+	{
+	}
+	
+
+}
+
+
+
